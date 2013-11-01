49f353c5d37c9ddac88c7d147ac4af6fc4d2dc3f
Ed Shadi <edshadi@gmail.com>
2013-11-01 14:52:11 -0700

    ajaxify the new menu form by using remote => true

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,11 +1,21 @@
 var Menu = {
   init: function() {
     $('a.new-menu').on('click', this.toggleMenuForm);
+    $('form#new_menu').on('ajax:success', this.appendMenu);
+    $('form#new_menu').on('ajax:error', this.appendErrors);
   },
 
   toggleMenuForm: function(e) {
     e.preventDefault();
     $('form#new_menu').toggleClass('hidden');
+  },
+
+  appendMenu: function(event, data, status, xhr) {
+    $('ul.menus').append(data.menu_template);
+  },
+
+  appendErrors: function(event, xhr, status, error) {
+    $('ul.menus').before($.parseJSON(xhr.responseText).error);
   }
 }
 
